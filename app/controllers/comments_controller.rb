e87a59366f32fc9783657454d7fc153d107f241c
Diego Ocampo <diegoocampo8@gmail.com>
2011-07-04 15:02:24 +0200

    aun no esta terminado

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,18 +3,37 @@ class CommentsController < ApplicationController
     @comment = Comment.new(params[:comment])
 
     if @comment.save
-      ; flash[:notice] = 'Comment was successfully created.'
-      redirect_to(posts_url)
+      flash[:notice] = 'Thanks for commenting!'
+      respond_to do |format|
+        format.html {redirect_to(posts_url)}
+        format.js
+      end
     else
       flash[:notice] = "Error creating comment: #{@comment.errors}"
-      redirect_to(posts_url)
+      respond_to do |format|
+        format.html {redirect_to(posts_url)}
+        format.js
+      end
     end
   end
 
-  def destroy
-    @comment = Comment.find(params[:id])
+  def eliminar
+
+    #Para especificar bien el comentario es necesario especificar tanto la id del comentario como la del post al q pertenece
+    @comment = Comment.find_by_id_and_post_id(params[:id],params[:post_id])
+
+    #ESTO SE PUEDE BORRAR ES PARA DEBUG
+    puts "tipo de clase: #{@comment.class}"
+    puts "tipo de superclase: #{@comment.class.superclass}"
+    ##################################
+
     @comment.destroy
 
-    redirect_to(posts_url)
+    puts "tipo de clase despues de eliminar: #{@comment.class}"
+    puts "id del objeto: #{@comment.id}"
+    respond_to do |format|
+      format.html {redirect_to(posts_url)}
+      format.js
+    end
   end
 end
